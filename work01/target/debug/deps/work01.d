d273753c4693ef4567908a451e101d8f10be320a
Happier <zhf5256@qq.com>
2025-04-28 22:21:56 +0800

    feat: 删除target

--- work01/target/debug/deps/work01.d
+++
@@ -1,5 +0,0 @@
-D:\code\rust-learn\work01\target\debug\deps\work01.exe: src\main.rs
-
-D:\code\rust-learn\work01\target\debug\deps\work01.d: src\main.rs
-
-src\main.rs:
--- work01/target/debug/work01.d
+++
@@ -1,1 +0,0 @@
-D:\code\rust-learn\work01\target\debug\work01.exe: D:\code\rust-learn\work01\src\main.rs
