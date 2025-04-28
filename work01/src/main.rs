4d48f6417ff22862c24abe324399444fcb0c4875
Happier <zhf5256@qq.com>
2025-04-28 22:20:26 +0800

    feat: 作业1

---
+++ work01/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+    let a: i32 = 10;
+    let b: u32 = 20;
+    // let c = a + b;
+    println!("a:{},b:{}", a, b,);
+
+    const THREE_AND_A_BIT: f32 = 3.4028236;
+    println!("{}", THREE_AND_A_BIT);
+}
---
+++ work01/target/debug/work01.d
@@ -0,0 +1,1 @@
+D:\code\rust-learn\work01\target\debug\work01.exe: D:\code\rust-learn\work01\src\main.rs
